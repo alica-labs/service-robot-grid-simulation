2e9fa473be98c1d5905ffd4a57092cd4949a4a95
Stephan Opfer <opfer@vs.uni-kassel.de>
2019-04-16 16:40:17 +0200

    - loading a custom png - copying it aside to the executable - get the relative path from /proc/self/exe

--- grid_sim_gui/src/GridSimGUI.cpp
+++ grid_sim_gui/src/GridSimGUI.cpp
@@ -2,11 +2,38 @@
 
 #include <SFML/Graphics.hpp>
 
-int main()
+#include <unistd.h>
+#include <limits.h>
+#include <iostream>
+
+std::string get_selfpath() {
+    char buff[PATH_MAX];
+    ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff)-1);
+    if (len != -1) {
+      buff[len] = '\0';
+      std::string exePath = std::string(buff);
+
+      return exePath.substr(0, exePath.find_last_of("/"));
+    }
+    /* handle error condition */
+}
+
+int main(int argc, char *argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(120, 120), "Grid Simulator GUI");
+
+    sf::Texture texture;
+    std::string executabelPath = get_selfpath();
+    std::cout << "[GridSimGUI] executabelPath: " << executabelPath << std::endl;
+    if (!texture.loadFromFile(executabelPath + "/textures/test_texture.png", sf::IntRect(30, 30, 60, 60))) {
+        std::cerr << "[GridSimGUI] Coudn't load the texture!" << std::endl;
+    }
+    texture.setSmooth(true);
+    texture.setRepeated(true);
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
 
     while (window.isOpen())
     {
@@ -18,7 +45,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 
